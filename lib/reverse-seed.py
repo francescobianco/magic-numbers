e5b2707fd68eac07ffd7719e24ac5b209cd7c213
francesco <info.francescobianco@gmail.com>
2024-01-19 16:18:34 +0100

    reverse seed

--- lib/reverse-seed.py
+++ lib/reverse-seed.py
@@ -2,6 +2,10 @@
 for a in range(10, 99):
 	for b in range(10, 99):
 		ab = str(a * b)
-		reverse_seed = str(b)[::-1] + str(a)[::-1]
+		straight_a = str(a)
+		straight_b = str(b)
+		reverse_a = str(a)[::-1]
+		reverse_b = str(b)[::-1]
+		reverse_seed = reverse_a + straight_b
 		if ab == reverse_seed:
 			print(a, b, ab, reverse_seed)
